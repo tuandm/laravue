0fc9cdc0d2f298e0e8c5639529410a0b70a52e22
Jeffrey Benistant <jj__bb@hotmail.com>
2019-09-19 16:24:05 +0200

    Updated project - Updated: Laravel 5.8 -> Laravel 6.0 - Updated: Element-ui 2.11.1 -> 2.12.0

--- app/User.php
+++ app/User.php
@@ -1,11 +1,15 @@
 <?php
+
 namespace App;
-use Illuminate\Notifications\Notifiable;
+
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
 class User extends Authenticatable
 {
     use Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -14,6 +18,7 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -22,6 +27,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
     /**
      * The attributes that should be cast to native types.
      *
