f465fff6ea9d8dc784c34d2537c120f47e005f17
sabatale <50220345+sabatale@users.noreply.github.com>
2020-04-04 17:48:04 -0400

    Fix Logo not being displayed.

--- resources/js/layout/components/Sidebar/index.vue
+++ resources/js/layout/components/Sidebar/index.vue
@@ -1,4 +1,6 @@
 <template>
+  <div :class="{'has-logo':showLogo}">
+  <logo v-if="showLogo" :collapse="isCollapse" />
   <el-scrollbar wrap-class="scrollbar-wrapper">
     <el-menu
       :show-timeout="200"
@@ -12,14 +14,17 @@
       <sidebar-item v-for="route in routes" :key="route.path" :item="route" :base-path="route.path" />
     </el-menu>
   </el-scrollbar>
+    </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
 import SidebarItem from './SidebarItem';
+import Logo from './Logo';
+import variables from '@/styles/variables.scss';
 
 export default {
-  components: { SidebarItem },
+  components: { SidebarItem, Logo },
   computed: {
     ...mapGetters([
       'sidebar',
@@ -28,6 +33,12 @@ export default {
     routes() {
       return this.$store.state.permission.routes;
     },
+    showLogo() {
+  return this.$store.state.settings.sidebarLogo
+},
+variables() {
+  return variables
+},
     isCollapse() {
       return !this.sidebar.opened;
     },
