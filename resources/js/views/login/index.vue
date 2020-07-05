558a3c3eaeb786e73b75a01fba1fe4afd2e3d205
Ali Ataf <aliattaf98@gmail.com>
2020-07-05 23:06:57 +0300

    fix: login responsive issue in small screen when landscape

--- resources/js/views/login/index.vue
+++ resources/js/views/login/index.vue
@@ -169,18 +169,19 @@ $bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
 .login-container {
-  position: fixed;
+  position: relative;
   height: 100%;
   width: 100%;
   background-color: $bg;
+  overflow-y: auto;
   .login-form {
     position: absolute;
-    left: 0;
-    right: 0;
+    left: 50%;
+    top: 50%;
     width: 520px;
     max-width: 100%;
     padding: 35px 35px 15px 35px;
-    margin: 120px auto;
+    transform: translate(-50%, -50%);
   }
   .tips {
     font-size: 14px;
